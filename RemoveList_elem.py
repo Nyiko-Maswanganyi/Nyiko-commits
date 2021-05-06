adad612ff2b646f38159febed2497687e264714f
Nyiko-Maswanganyi <nyikotaker@gmail.com>
2021-05-06 21:38:32 +0200

    Create RemoveList_elem.py
      a function to remove and print every third number from a list of numbers until the list becomes empty.

---
+++ RemoveList_elem.py
@@ -0,0 +1,11 @@
+def remover(ex_list):
+  #list starts with 0 index
+  position = 3 - 1 
+  idx = 0
+  size_list = (len(int_list))
+  while len_list>0:
+    idx = (position+idx)%size_list
+    print(int_list.pop(idx))
+    size_list -= 1
+ 
+ 
